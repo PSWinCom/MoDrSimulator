79d70111f218b02b32e2cd276130d958d04bd34c
Henrik Grotle <henrik@pswin.com>
2014-07-04 17:22:15 +0200

    improved event

--- index.js
+++ index.js
@@ -6,69 +6,51 @@ var app = express();
 
 var mo = require("./mo");
 
-var basicAuth = function(req, res, next) {
-  var header=req.headers['authorization']||'',        // get the header
-        token=header.split(/\s+/).pop()||'',            // and the encoded auth token
-        auth=new Buffer(token, 'base64').toString(),    // convert from base64
-        parts=auth.split(/:/),                          // split on colon
-        username=parts[0],
-        password=parts[1];
+var basicAuth = function(sendevent) { 
+  return function(req, res, next) {
+    var header=req.headers['authorization']||'',        // get the header
+          token=header.split(/\s+/).pop()||'',            // and the encoded auth token
+          auth=new Buffer(token, 'base64').toString(),    // convert from base64
+          parts=auth.split(/:/),                          // split on colon
+          username=parts[0],
+          password=parts[1];
 
-  console.log(req.headers);
-  console.log('username is "'+username+'" and password is "'+password+'"');
-  
-  var statusCode = 200;
-    if (username != "test" || password != "password")
-      statusCode = 401;
+    console.log(req.headers);
+    console.log('username is "'+username+'" and password is "'+password+'"');
+    
+    var statusCode = 200;
+      if (username != "test" || password != "password")
+        statusCode = 401;
 
-  if (statusCode == 200)
-    next();
-  else
-    next("Unauthorized");
+    if (statusCode == 200)
+      next();
+    else {
+      sendevent({ type: 'authreject', body: { username: username, password: password } });
+      res.send(statusCode, "Unauthorized");
+    }
+  }
 };
+
+var events = [];
 
 app.set("port", port);
 app.use(require("express-xml-bodyparser")());
 
 app.get("/", function(req, res) {
-  res.send(mo.list());
+  res.send(JSON.stringify(events));
 });
 
-app.post("/xml/mo/secure", basicAuth, mo.receive);
-app.post("/xml/mo", mo.receive);
+var eventreceived = function (event) {
+  event.time = new Date();
+  events.push(event);
+};
+
+app.post("/dr/secure", basicAuth(eventreceived), mo.receivedr(eventreceived));
+app.post("/dr", mo.receivedr(eventreceived));
+
+app.post("/mo/secure", basicAuth(eventreceived), mo.receivemo(eventreceived));
+app.post("/mo", mo.receivemo(eventreceived));
 
 var server = app.listen(app.get("port"), function() {
   console.log('Listening on port %d', server.address().port);
 });
-
-/*var server = http.createServer(function(req,res) {
-  var header=req.headers['authorization']||'',        // get the header
-      token=header.split(/\s+/).pop()||'',            // and the encoded auth token
-      auth=new Buffer(token, 'base64').toString(),    // convert from base64
-      parts=auth.split(/:/),                          // split on colon
-      username=parts[0],
-      password=parts[1];
-
-  console.log('username is "'+username+'" and password is "'+password+'"');
-
-  var statusCode = 200;
-  if (username != "test" || password != "password")
-    statusCode = 401;
-
-  res.writeHead(statusCode,{'Content-Type':'text/xml'});
-  if (statusCode == 200)
-    res.end('<?xml version="1.0"?> \
-      <MSGLST> \
-        <MSG> \
-          <ID>1</ID> \
-          <STATUS>OK</STATUS> \
-        </MSG> \
-      </MSGLST>');
-  else
-    res.end("Unauthorized");
-});
-
-server.listen(port, function () {
-  console.log("process.env.PORT = " + process.env.PORT);
-  console.log("Listening for MO requests on port " + port);
-});*/
--- mo.js
+++ mo.js
@@ -1,8 +1,17 @@
-var mos = [];
+exports.receivemo = function(callback) {
+  return function(req, res) {
+    callback({ event: 'mo', body: req.body.msglst.msg[0] });
+    //res.send("<!DOCTYPE MSGLST SYSTEM \"pswincom_report_response.dtd\">");
+    res.send({ MSGLST: { MSG: [ { ID: 1, STATUS: "OK" } ] } });
+  }
+}
 
-exports.receive = function(req, res) {
-  mos.push(req.body.msglst.msg[0]);
-  res.send({ });
+exports.receivedr = function(callback) {
+  return function(req, res) {
+    callback({ event: 'dr', body: req.body.msglst.msg[0] });
+    //res.send("<!DOCTYPE MSGLST SYSTEM \"pswincom_report_response.dtd\">");
+    res.send({ MSGLST: { MSG: [ { ID: 1, STATUS: "OK" } ] } });
+  }
 }
 
 exports.list = function() {
