bf4574788a629f9e26baf604e782ee0a926cbc3e
Henrik Grotle <henrik@pswin.com>
2014-07-08 10:58:44 +0200

    sending mo/dr reply as xml

--- mo.js
+++ mo.js
@@ -2,7 +2,11 @@ exports.receive = function(type, callback) {
   return function(req, res) {
     console.log(req.body);
     callback({ event: type, body: req.body.MSGLST.MSG[0] || req.body.MSGLST.MSG });
-    res.send({ MSGLST: { MSG: [ { ID: 1, STATUS: "OK" } ] } });
+
+    var xml2js = require('xml2js');
+    var builder = new xml2js.Builder();
+    res.set('Content-Type', 'application/xml');
+    res.send(builder.buildObject({ MSGLST: { MSG: [ { ID: 1, STATUS: "OK" } ] } }));
   }
 };
 
