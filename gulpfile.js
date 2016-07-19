9a972099bddd5ce6cfc821338835e5fcf9750ff7
Henrik Grotle <henrik.grotle@linkmobility.com>
2016-07-19 10:56:48 +0200

    environment dependent gulp uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,22 @@ var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
+var gutil = require('gulp-util');
 
 gulp.task('default', function() {
 });
 
-gulp.task('bootstrap', function() {
+gulp.task('bootstrap-fonts', function() {
   gulp.src('src/static/bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg,woff2}')
     .pipe(gulp.dest("dist/static/fonts"));
 });
 
-gulp.task('dev', ['bootstrap'], function() {
+gulp.task('default', ['bootstrap-fonts'], function() {
     gulp.src('src/static/index.html')
       .pipe(usemin({
             assetsDir: 'src/static',
-            css: ['concat'],
-            js: ['concat']
-        }))
-      .pipe(
-        gulp.dest("dist/static"));
-});
-
-gulp.task('prod', ['bootstrap'], function() {
-    gulp.src('src/static/index.html')
-      .pipe(usemin({
-            assetsDir: 'src/static',
-            css: [minifyCss(), 'concat'],
-            js: [uglify(), 'concat']
+            css: gutil.env.env === 'prod' ? [minifyCss(), 'concat'] : [ 'concat' ],
+            js: gutil.env.env === 'prod' ? [uglify(), 'concat'] : [ 'concat' ]
         }))
       .pipe(
         gulp.dest("dist/static"));
