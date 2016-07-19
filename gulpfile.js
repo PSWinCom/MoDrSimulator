1dbfb02d665f5851fc338b33f6272bb17568cd69
Henrik Grotle <henrik.grotle@linkmobility.com>
2016-07-19 11:02:40 +0200

    running bower in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,23 @@ var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var gutil = require('gulp-util');
+var bower = require('gulp-bower');
 
-gulp.task('default', function() {
+gulp.task('bower', function() {
+  return bower();
 });
 
-gulp.task('bootstrap-fonts', function() {
+gulp.task('bootstrap-fonts', [ 'bower' ], function() {
   gulp.src('src/static/bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg,woff2}')
     .pipe(gulp.dest("dist/static/fonts"));
 });
 
-gulp.task('default', ['bootstrap-fonts'], function() {
+gulp.task('default', ['bower', 'bootstrap-fonts'], function() {
     gulp.src('src/static/index.html')
       .pipe(usemin({
             assetsDir: 'src/static',
-            css: gutil.env.env === 'prod' ? [minifyCss(), 'concat'] : [ 'concat' ],
-            js: gutil.env.env === 'prod' ? [uglify(), 'concat'] : [ 'concat' ]
+            css: (gutil.env.NODE_ENV === 'production') ? [minifyCss(), 'concat'] : [ 'concat' ],
+            js: (gutil.env.NODE_ENV === 'production') ? [uglify(), 'concat'] : [ 'concat' ]
         }))
       .pipe(
         gulp.dest("dist/static"));
