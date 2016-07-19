9af842efb78fb5dbff7f33827326968cfe300e79
Henrik Grotle <henrik.grotle@linkmobility.com>
2016-07-19 13:43:50 +0200

    using moment.js to display received time

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 
 angular.module('modrApp', [
+  'angularMoment',
   'ui.bootstrap',
   'modrApp.controllers',
   'modrApp.services',
--- src/static/js/directives.js
+++ src/static/js/directives.js
@@ -5,7 +5,7 @@
 angular
   .module('modrApp.directives', [])
   .directive('eventDetail', function($compile) {
-    var moTemplate = '<div><div><span class="label label-primary">{{content.body.SND}}</span> <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> </span><span class="label label-default">{{content.body.RCV}}</span></div><div style="font-size: 200%">{{content.body.TEXT || content.body.TXT}}</div></div>';
+    var moTemplate = '<div><div><span class="label label-primary">{{content.body.SND}}</span> <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> </span><span class="label label-default">{{content.body.RCV}}</span></div><div style="font-size: 3vw">{{content.body.TEXT || content.body.TXT}}</div></div>';
     var drTemplate = '<div><span class="label label-primary">{{content.body.RCV}}</span> <span class="label label-default">{{content.body.STATE}}</span><div>{{content.body.REF}}</div></div>';
     var rejectTemplate = '<div>Rejected: {{content.body.username}}:{{content.body.password}}</div>';    
 
